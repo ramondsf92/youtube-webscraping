6a4fde7ac03697da4b26459180d768f498e70d5d
FlameThrow <r2flamethrow@gmail.com>
2020-08-25 18:55:48 -0300

    Testing with selenium Chrome Driver

--- webscraping-youtube.py
+++ webscraping-youtube.py
@@ -1,31 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import webbrowser
 
-def vids_list(search):
-    url = 'https://www.youtube.com/results?search_query=' + search
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    a = soup.find_all("a", {'class': "yt-uix-tile-link"})
-    vid_dict = {}
-    for i in a:  # run the a list
-        if len(vid_dict) > 6:
-            break
-        else:
-            vid_name = i['title']
-            watch_link = i['href']
-            final_link = 'https://www.youtube.com/' + watch_link
-            vid_dict[vid_name] = final_link
-        
-    return vid_dict
+busca = input("Insira o nome da música a ser buscada: ")
 
-search = input('Digite o nome da música: ').strip().replace(' ', '+')
-videos = vids_list(search)
-titles = []  # Lista para armazenar os títulos ordenadamente
+driver = webdriver.Chrome()
+driver.get("https://www.youtube.com/results?search_query=" + busca.strip().replace(' ', '+'))
 
-for c,key in zip(range(len(videos)),videos.keys()): # Guarda os títulos na lista e printa em ordem
-    titles.append(key)
-    print(f'{c+1} - {key}')
-op = int(input('Qual video deseja abrir? '))
-final_url = videos[titles[op-1]]
-webbrowser.open_new_tab(final_url)
+user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
+titulos = []
+links = []
+for i in user_data:
+    titulos.append(i.get_attribute('title'))
+    links.append(i.get_attribute('href'))
+
+print(len(links))
+print(titulos[:5])
+driver.get(links[0])
+# webbrowser.open_new_tab(links[0])
+
